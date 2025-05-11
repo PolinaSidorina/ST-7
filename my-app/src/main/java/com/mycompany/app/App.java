734a28fd3db8fd0736ddf2375acae93c8c4a71c9
Polina Sidorina <polishas206@gmail.com>
2025-05-11 14:19:01 +0300

    0.2

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,11 +1,4 @@
 package com.mycompany.app;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App
 {
